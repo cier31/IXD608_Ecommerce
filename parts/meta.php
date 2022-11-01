9176df781732fe8294078006d2e134751f410b87
cier31 <112988395+cier31@users.noreply.github.com>
2022-10-31 22:59:57 -0700

    Add files via upload

---
+++ parts/meta.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<base href="http://mengxixiao.com/ixd608/m2/">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="css/storetheme.css">
