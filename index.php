1257a7ef4ba50310014561e494bfdd22d66cd634
cier31 <112988395+cier31@users.noreply.github.com>
2022-11-21 23:19:43 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?
+	include_once "lib/php/functions.php";
+	// print_p($_SESSION,$_GET,$_POST);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Doggies' Storage</title>
+	<? include "parts/meta.php"; ?>
+
+</head>
+<body>
+
+	<? include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			
+			<h2>Home Page</h2>
+
+			<p>Create a home page here.</p>
+				
+				
+		</div>
+	</div>
+
+	
+</body>
+</html>
