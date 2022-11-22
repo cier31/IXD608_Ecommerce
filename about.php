48a324081ef15f499175dba4b87d81156645dcec
cier31 <112988395+cier31@users.noreply.github.com>
2022-11-21 23:17:57 -0800

    Update about.php

--- about.php
+++ about.php
@@ -1,18 +1,28 @@
+<?
+	include_once "lib/php/functions.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 
-    <title>About Page</title>
-    <? include "parts/meta.php";?>
+	<title>About Us</title>
+	<? include "parts/meta.php"; ?>
+
 </head>
+
 <body>
-     <? include "parts/navbar.php";?>
-    <div class="container">
-        <div class="card soft">
-            <h2>About Us </h2>
-                <p>This should be a page where you write about your company</p>
-           
-        </div>
-    </div>
+
+	<? include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About Us</h2>
+			<p>This should be a page where you write about your company.</p>
+		</div>
+	</div>
+
+
 </body>
+
 </html>
