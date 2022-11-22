5c716c58b5d1dab6c825f926adc0a4dd05a462b6
cier31 <112988395+cier31@users.noreply.github.com>
2022-11-21 23:21:19 -0800

    Add files via upload

---
+++ lib/php/auth.php
@@ -0,0 +1,13 @@
+<?
+function MYSQLIAuth() {
+	return [
+		"localhost", //localhost
+		"mengxi331", //your database user name
+		"976869Ab", //database password
+		"ixd608_1" //database name
+	];
+}
+
+
+
+?>
